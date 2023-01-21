aa21615f4ae52b2347b5693b7b7d08ac929d3b16
lytMoon <2936437824@qq.com>
2023-01-21 21:11:56 +0800

    第三次提交，降低了SDK版本，实现了文本阅读的翻页效果和进度显示

--- app/src/main/java/com/example/funread/BookActivity.java
+++ app/src/main/java/com/example/funread/BookActivity.java
@@ -4,11 +4,20 @@ package com.example.funread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import com.example.funread.view.BookPageBezierHelper;
+import com.example.funread.view.BookPageView;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,22 +26,70 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
-public class BookActivity extends AppCompatActivity {
+public class BookActivity extends Activity {
+    private BookPageBezierHelper mHelper;
+    private BookPageView mBookPageView;
 
     private static final String FILE_PATH = "TEST";
     private String mFilePath;
     private TextView mTextView;
+    private int mWidth;
+    private int mHeight;
+    private Bitmap mCurrentPageBitmap;
+    private Bitmap mNextPageBitmap;
+    private int mCurrentLength;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        // init View
+        mBookPageView = findViewById(R.id.book_page_view);
+        mTextView = findViewById(R.id.progress_text_view);
+
+        //开启全屏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);//开启全屏
+
+
+        Log.d("mFilePath", "(BookActivity.java:23)-->> " + mFilePath);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mWidth = displayMetrics.widthPixels;
+        mHeight = displayMetrics.heightPixels;
+        final BookPageBezierHelper helper = new BookPageBezierHelper(mWidth,mHeight);
+        mBookPageView.setBookPageBezierHelper(helper);
+        mCurrentPageBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        mNextPageBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        mBookPageView.setBitmaps(mCurrentPageBitmap, mNextPageBitmap);
+
+       //设置背景
+        helper.setBackground(this,R.drawable.background_read);
+
+        //设置进度
+        helper.setOnProgressChangedListener(new BookPageBezierHelper.OnProgressChangedListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void setProgress(int currentLength, int totalLength) {
+                mCurrentLength = currentLength;
+                float progress = mCurrentLength * 100 / totalLength;
+                mTextView.setText(String.format("%s%%", progress));
+            }
+        });
+
         if (getIntent() != null) {
             mFilePath = getIntent().getStringExtra(FILE_PATH);
-        }
-        Log.d("mFilePath", "(BookActivity.java:23)-->> " + mFilePath);
+            if (!TextUtils.isEmpty(mFilePath)){
+                try {
+                    helper.openBook(mFilePath);
+                    helper.draw(new Canvas(mCurrentPageBitmap));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
+            }
+        }
     }
 
     public static void start(Context context, String filePath) throws IOException {
